d4bdda547122873a35afca5907bf4eeea723d98d
RezaNoel <88496269+RezaNoel@users.noreply.github.com>
2023-03-11 12:17:27 +0330

    Version-1-2

--- __resource.lua
+++ __resource.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Have Good Time..!'
 description 'Have Fun..!'
-version '1.0'
+version '1.2'
 
 resource_type 'gametype' { name = 'Noel Awesome Sources!' }
 
--- main.lua
+++ main.lua
@@ -1,26 +1,57 @@
+-- Created by RezaNoel#0001
+-- Version 1.2
+-- _  _  _____  ____  __   
+--( \( )(  _  )( ___)(  )  
+-- )  (  )(_)(  )__)  )(__ 
+--(_)\_)(_____)(____)(____)
+  
 
-function creatblip(name,radios,r,g,b)
+i=1
+function creatblip(name,color)
   CreateThread(function()
-	  Wait(1)
-    local color={r=r,g=g,b=b}
-    -- color.r=r
-    -- color.g=g
-    -- color.b=b
+    if blips[i] then
+    print("if-1 "..i)
+    SetBlipSprite(blips[i].bblip, 0)
+    SetBlipAsShortRange(blips[i].bblip, true)
+    SetBlipColour(blips[i].bblip, blips[i].bcolor)
+    SetBlipScale(blips[i].bblip, 1.0)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString(blips[i].bname)
+    EndTextCommandSetBlipName(blips[i].bblip)
+    SetBlipAlpha(blips[i].bradius, 100)
+    SetBlipColour(blips[i].bradius, blips[i].bcolor)
+      i = i + 1
+      print("if-2 "..i)
+    else
+    
+    print("else "..i)
+   
     local pedcoords=GetEntityCoords(PlayerPedId())
     blip = AddBlipForCoord(pedcoords.x, pedcoords.y, pedcoords.z)
-    radiusBlip = AddBlipForRadius(pedcoords.x, pedcoords.y, pedcoords.z,radios)
+    radiusBlip = AddBlipForRadius(pedcoords.x, pedcoords.y, pedcoords.z,100.0)
+    blips[i]={bname=name,bcolor=color,bblip=blip,bradius=radiusBlip}
     SetBlipSprite(blip, 0)
     SetBlipAsShortRange(blip, true)
-    SetBlipColour(blip, color)
+    SetBlipColour(blip, blips[i].bcolor)
     SetBlipScale(blip, 1.0)
     BeginTextCommandSetBlipName('STRING')
-    AddTextComponentString(name)
+    AddTextComponentString(blips[i].bname)
     EndTextCommandSetBlipName(blip)
     SetBlipAlpha(radiusBlip, 100)
-    SetBlipColour(radiusBlip, color)
-
+    SetBlipColour(radiusBlip, blips[i].bcolor)
+    
+    i = i + 1
+    end
+    
   end)
 end
 
+CreateThread(function()
+TriggerEvent('chat:addSuggestion', '/cblip', 'name , color')
 
-RegisterCommand("cblip", function (source,args)creatblip(args[1],args[2],args[3],args[4],args[5]) end, false)
+end)
+
+RegisterCommand("cblip", function (source,args)creatblip(args[1],args[2])
+
+end, false)
+
